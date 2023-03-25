e50150f1a4e245a66d926ef82f20b9048ed4cedc
MaryKaheti <kahetikihara@gmail.com>
2023-03-25 17:09:54 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('#list').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.new_todo.value)
+  })
 });
+function handleToDo(todo) {
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.textContent = "x"
+  p.textContent = `${todo}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#todo_container').appendChild(p)
+}
+function displayData() {
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let form = event.target.description.value
+  })
+}
+displayData()
